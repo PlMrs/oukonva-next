673f46ef245633cf2ff1cc0e3c0d37b7f9ec636d
paul <paulmaraispro@gmail.com>
2023-09-28 16:20:32 +0200

    feat: add openai

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server"
+import { openAI } from "../config/openai";
 
 export async function POST(request: Request) {
-     const result = "salut internet"
 
-     return NextResponse.json(result);
+     const body = await request.json();
+
+     const chatCompletion = await openAI.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+      });
+
+     return NextResponse.json(chatCompletion);
 }
